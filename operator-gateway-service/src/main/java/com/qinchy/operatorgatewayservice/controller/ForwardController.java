1ac045bb52d8f25c87173bdd0344243bbf25fe21
qinchy <qinchy@msn.com>
2023-04-27 18:37:27 +0800

    init commit

--- operator-gateway-service/src/main/java/com/qinchy/operatorgatewayservice/controller/ForwardController.java
+++ operator-gateway-service/src/main/java/com/qinchy/operatorgatewayservice/controller/ForwardController.java
@@ -8,7 +8,11 @@ import com.jayway.jsonpath.JsonPath;
 import com.qinchy.operatorgatewayservice.bean.Response;
 import com.qinchy.operatorgatewayservice.config.OperatorConfig;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.*;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -42,14 +46,19 @@ public class ForwardController {
     @Value("${forward-json-template}")
     private String forwardJsonTemplate;
 
-    @Value("${handler-expression}")
-    private String handlerExpress;
+    @Value("${forward-handler-expression}")
+    private String forwardHandlerExpress;
+
+    @Value("${response-json-template}")
+    private String responseJsonTemplate;
+
+    @Value("${response-handler-expression}")
+    private String responseHandlerExpress;
 
     @PostMapping("/forward")
     public Response<Map<String, Object>> forward(@RequestBody Map<String, Object> request) throws Exception {
-
         log.info("转发JSON模板配置：{}", forwardJsonTemplate);
-        log.info("需要执行的动作模板配置：{}", handlerExpress);
+        log.info("需要执行的动作模板配置：{}", forwardHandlerExpress);
 
         log.info("接收到内部请求报文：{}", request);
         String receiver = (String) request.get("receiver");
@@ -59,59 +68,38 @@ public class ForwardController {
         String host = operatorConfig.getOperatorConfig().get(receiver).get("host");
         log.info("获取到 {} 的主机地址为：{}", receiver, host);
 
-        // 将转发的JSON模板转换成文档上下文对象，后面对此对象进行操作。
-        DocumentContext documentContext = JsonPath.parse(forwardJsonTemplate);
-        log.info("转发的JSON文档为：{}", documentContext.jsonString());
-        EvaluationContext evaluationContext = getEvaluationContext(request);
-        Map<String, Object> result = buildSendMessage(documentContext, evaluationContext);
+        Map<String, Object> forwardMessage = buildForwardMessage(request);
+
+        log.info("开始请求运营商。。。");
+
+        // TODO Mock Response Message
+        JSONObject operatorResponseMessage = mockResponseMessage();
+        log.info("运营商返回：{}", operatorResponseMessage);
+
+        Map<String, Object> responseMessage = buildResponseMessage(operatorResponseMessage);
 
         // 构造返回对象
         Response.ResponseBuilder<Map<String, Object>> builder = Response.builder();
-
         builder.success(true);
         builder.desc("success");
         builder.code("0");
-
-        builder.data(result);
+        builder.data(responseMessage);
 
         return builder.build();
     }
 
     /**
-     * 构造变量值上下文
+     * 返回给内部dubbo接口的信息
      *
-     * @param request 请求的map
-     * @return 返回变量上下文
+     * @param operatorResponseMessage 运营商返回信息
+     * @return 内部dubbo接口的信息
      */
-    private EvaluationContext getEvaluationContext(Map<String, Object> request) {
-        EvaluationContext evaluationContext = new StandardEvaluationContext();
+    private Map<String, Object> buildResponseMessage(JSONObject operatorResponseMessage) throws Exception {
+        DocumentContext responseDocumentContext = JsonPath.parse(responseJsonTemplate);
+        log.info("转发的JSON文档为：{}", responseDocumentContext.jsonString());
+        EvaluationContext evaluationContext = getResponseEvaluationContext(operatorResponseMessage);
 
-        String clientId = operatorConfig.getOperatorConfig().get((String) request.get("receiver")).get("clientid");
-        evaluationContext.setVariable("clientId", clientId);
-
-        String senderDateTime = DateFormatUtils.format(new Date(), "yyyy-MM-dd'T'HH:mm:ss");
-        evaluationContext.setVariable("senderDateTime", senderDateTime);
-
-        String messageSn = (String) request.get("msgSn");
-        evaluationContext.setVariable("messageSn", messageSn);
-        evaluationContext.setVariable("signatureSn", "signatureSn");
-        evaluationContext.setVariable("encryptionSn", "encryptionSn");
-        evaluationContext.setVariable("digitalEnvelope", "digitalEnvelope");
-        evaluationContext.setVariable("iv", "iv");
-        evaluationContext.setVariable("request", request);
-        return evaluationContext;
-    }
-
-    /**
-     * 构造发送给运营商的请求
-     *
-     * @param documentContext   模板上下文
-     * @param evaluationContext 变量上下文
-     * @return 发送给运营商的请求内容
-     * @throws Exception 抛出异常
-     */
-    private Map buildSendMessage(DocumentContext documentContext, EvaluationContext evaluationContext) throws Exception {
-        JSONArray jsonArray = JSON.parseArray(handlerExpress);
+        JSONArray jsonArray = JSON.parseArray(responseHandlerExpress);
         if (jsonArray.size() == 0) {
             throw new Exception("配置为空");
         }
@@ -139,17 +127,135 @@ public class ForwardController {
 
             // 添加或者修改指定路径的值(父路径不存在时不添加)
             if ("modify".equalsIgnoreCase(action)) {
-                documentContext.set(jsonPath, value);
+                responseDocumentContext.set(jsonPath, value);
             }
         }
 
-        String result1 = doSyncPost("http://localhost:8080/echo", documentContext.jsonString());
+        return JSON.parseObject(responseDocumentContext.jsonString(), Map.class);
+    }
+
+    private EvaluationContext getResponseEvaluationContext(JSONObject operatorResponseMessage) {
+        EvaluationContext evaluationContext = new StandardEvaluationContext();
+
+
+        Object msgHeaderObject = operatorResponseMessage.get("msg_header");
+        if (msgHeaderObject instanceof JSONObject){
+            JSONObject msgHeaderJsonobject = (JSONObject) msgHeaderObject;
+            evaluationContext.setVariable("sender", (String) msgHeaderJsonobject.get("sender"));
+            evaluationContext.setVariable("receiver", (String) msgHeaderJsonobject.get("receiver"));
+            evaluationContext.setVariable("msgType", (String) msgHeaderJsonobject.get("msgType"));
+            evaluationContext.setVariable("msgSn", msgHeaderJsonobject.get("message_sn"));
+        }
+
+        evaluationContext.setVariable("msgBody", operatorResponseMessage.get("msg_body"));
+
+        return evaluationContext;
+    }
+
+    /**
+     * mock运营商返回的信息
+     *
+     * @return 运营商返回的信息
+     */
+    private JSONObject mockResponseMessage() {
+        String json = "{\n" +
+                "\t\"msg_header\": {\n" +
+                "\t\t\"version\": \"01\",\n" +
+                "\t\t\"sender_date_time\": \"2023-04-27T11:48:08\",\n" +
+                "\t\t\"message_sn\": \"202304201120370001234567\",\n" +
+                "\t\t\"client_id\": \"cmccclientid\",\n" +
+                "\t\t\"signature_sn\": \"signatureSn\",\n" +
+                "\t\t\"encryption_sn\": \"encryptionSn\",\n" +
+                "\t\t\"digital_envelope\": \"digitalEnvelope\",\n" +
+                "\t\t\"iv\": \"iv\"\n" +
+                "\t},\n" +
+                "\t\"msg_body\": {\n" +
+                "\t\t\"return_status\": \"S\",\n" +
+                "\t\t\"return_code\": \"0000\",\n" +
+                "\t\t\"return_message\": \"Success\",\n" +
+                "\t\t\"mno\": \"1\"\n" +
+                "\t}\n" +
+                "}";
+        return JSONObject.parseObject(json);
+    }
+
+    /**
+     * 构造变量值上下文
+     *
+     * @param request 请求的map
+     * @return 返回变量上下文
+     */
+    private EvaluationContext getForwardEvaluationContext(Map<String, Object> request) {
+        EvaluationContext evaluationContext = new StandardEvaluationContext();
+
+        String clientId = operatorConfig.getOperatorConfig().get((String) request.get("receiver")).get("clientid");
+        evaluationContext.setVariable("clientId", clientId);
+
+        String senderDateTime = DateFormatUtils.format(new Date(), "yyyy-MM-dd'T'HH:mm:ss");
+        evaluationContext.setVariable("senderDateTime", senderDateTime);
+
+        String messageSn = (String) request.get("msgSn");
+        evaluationContext.setVariable("messageSn", messageSn);
+        evaluationContext.setVariable("signatureSn", "signatureSn");
+        evaluationContext.setVariable("encryptionSn", "encryptionSn");
+        evaluationContext.setVariable("digitalEnvelope", "digitalEnvelope");
+        evaluationContext.setVariable("iv", "iv");
+        evaluationContext.setVariable("request", request);
+        return evaluationContext;
+    }
+
+    /**
+     * 构造发送给运营商的请求
+     *
+     * @param request 内部请求信息
+     * @return 发送给运营商的请求内容
+     * @throws Exception 抛出异常
+     */
+    private Map buildForwardMessage(Map<String, Object> request) throws Exception {
+        // 将转发的JSON模板转换成文档上下文对象，后面对此对象进行操作。
+        DocumentContext forwardDocumentContext = JsonPath.parse(forwardJsonTemplate);
+        log.info("转发的JSON文档为：{}", forwardDocumentContext.jsonString());
+        EvaluationContext evaluationContext = getForwardEvaluationContext(request);
+
+        JSONArray jsonArray = JSON.parseArray(forwardHandlerExpress);
+        if (jsonArray.size() == 0) {
+            throw new Exception("配置为空");
+        }
+
+        //创建ExpressionParser解析表达式
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp;
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            JsonPath jsonPath = JsonPath.compile(jsonObject.getString("jsonpath"));
+            String action = jsonObject.getString("action");
+            String valueOrExpress = jsonObject.getString("value");
+
+            // 默认是字面值
+            Object value = valueOrExpress;
+            try {
+                exp = parser.parseExpression(valueOrExpress);
+                Class<?> valueType = exp.getValueType(evaluationContext);
+                value = exp.getValue(evaluationContext, valueType);
+            } catch (ParseException e) {
+                log.error("解析SpEL表达式{}异常，值可能是字面文本值", valueOrExpress, e);
+            } catch (SpelEvaluationException e) {
+                log.error("取值SpEL表达式{}异常，值可能是字面文本值", valueOrExpress, e);
+            }
+
+            // 添加或者修改指定路径的值(父路径不存在时不添加)
+            if ("modify".equalsIgnoreCase(action)) {
+                forwardDocumentContext.set(jsonPath, value);
+            }
+        }
+
+        String result1 = doSyncPost("http://localhost:8080/echo", forwardDocumentContext.jsonString());
         log.info("OkHttp同步请求结果：{}", result1);
 
-        String result2 = doAsyncPost("http://localhost:8080/echo", documentContext.jsonString());
+        String result2 = doAsyncPost("http://localhost:8080/echo", forwardDocumentContext.jsonString());
         log.info("OkHttp异步请求结果：{}", result2);
 
-        return JSON.parseObject(documentContext.jsonString(), Map.class);
+        return JSON.parseObject(forwardDocumentContext.jsonString(), Map.class);
     }
 
     /**
